0219acbb4943134d9d265798e68162e25f3f4694
Kyle Tolle <kyle@nullsix.com>
2023-01-11 12:52:58 -0700

    Reuse random letter generation in simple strategy

--- strategies/simple.ts
+++ strategies/simple.ts
@@ -1,28 +1,28 @@
 import { randomNumber } from "https://deno.land/x/random_number@2.0.0/mod.ts";
-import { consonants, vowels, LetterTypes } from "../constants.ts";
+import { LetterTypes } from "../constants.ts";
+import type { ISimpleLetterTypeWeights, IFullLetterTypeWeights } from "../interfaces.ts";
+import { convertSimpleToFullLetterTypeWeightedRange, generateWeightedLetterFromLetterTypeWeights } from "../weights/index.ts";
 
 const generateRandomName = () => {
   const { consonant, vowel } = LetterTypes;
 
+  const simpleLetterTypeEqualWeights : ISimpleLetterTypeWeights = {
+    [consonant]: { length: 50 },
+    [vowel]: { length: 50 },
+  };
+  const letterTypeWeightedEqually: IFullLetterTypeWeights = convertSimpleToFullLetterTypeWeightedRange(simpleLetterTypeEqualWeights);
+
   const nameLength = randomNumber({ min: 2, max: 10});
   const letters: string[] = [];
-  for(let i = 0; i < nameLength; i++) {
-    const randomLetterTypeValue = Math.round(randomNumber({min: 0, max: 1, integer: false}));
-    const randomLetterType= randomLetterTypeValue === 1 ? consonant: vowel;
 
-    let randomLetter: string;
-    switch (randomLetterType) {
-      case consonant:
-        randomLetter = consonants[randomNumber({min: 0, max: consonants.length - 1})]
-        break;
-      case vowel:
-        randomLetter = vowels[randomNumber({min: 0, max: vowels.length - 1})]
-        break;
-    }
+  for(let i = 0; i < nameLength; i++) {
+    const randomLetter = generateWeightedLetterFromLetterTypeWeights(letterTypeWeightedEqually);
     letters.push(randomLetter);
   }
+
   letters[0] = letters[0].toUpperCase();
   const randomName = letters.join('');
+
   return randomName;
 }
 
