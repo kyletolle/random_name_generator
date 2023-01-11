2ac5806c1afaac6ad2de61ab59ec25d68f9adaf6
Kyle Tolle <kyle@nullsix.com>
2023-01-11 12:52:40 -0700

    Extract some random letter code for reuse

--- strategies/basicWeighted.ts
+++ strategies/basicWeighted.ts
@@ -1,37 +1,10 @@
 import { randomNumber } from "https://deno.land/x/random_number@2.0.0/mod.ts";
-import { consonants, vowels, LetterTypes } from "../constants.ts";
-import type { TLetterTypes } from "../constants.ts";
+import { vowels, LetterTypes } from "../constants.ts";
 import type { ISimpleLetterTypeWeights, IFullLetterTypeWeights } from "../interfaces.ts";
+import { convertSimpleToFullLetterTypeWeightedRange, generateWeightedLetterFromLetterTypeWeights } from "../weights/index.ts";
 
 const generateRandomName = () => {
   const { consonant, vowel } = LetterTypes;
-
-  const getRandomLetterFromLetterType = (letterType: TLetterTypes) => {
-    switch (letterType) {
-      case consonant:
-        return consonants[randomNumber({min: 0, max: consonants.length - 1})]
-      default:
-        return vowels[randomNumber({min: 0, max: vowels.length - 1})]
-    }
-  }
-
-  const convertSimpleToFullLetterTypeWeightedRange = (simpleWeights: ISimpleLetterTypeWeights): IFullLetterTypeWeights => {
-    let lastLength = 0;
-    const fullWeights: IFullLetterTypeWeights = {
-      [consonant]: { start: 0, end: 0 },
-      [vowel]: { start: 0, end: 0 },
-    };
-    for (const simpleWeightKey in simpleWeights) {
-      const simpleWeight = simpleWeights[simpleWeightKey as TLetterTypes];
-      const start = lastLength + 1;
-      const end = lastLength + simpleWeight.length;
-      lastLength = end;
-      const fullRange = { start, end };
-      fullWeights[simpleWeightKey as TLetterTypes] = fullRange;
-    }
-
-    return fullWeights;
-  }
 
   const simpleLetterTypeWeightedTowardConsonant: ISimpleLetterTypeWeights = {
     [consonant]: { length: 95 },
@@ -44,27 +17,6 @@ const generateRandomName = () => {
     [vowel]: { length: 95 },
   }
   const letterTypeWeightedTowardVowel: IFullLetterTypeWeights = convertSimpleToFullLetterTypeWeightedRange(simpleLetterTypeWeightedTowardVowel);
-
-  const generateWeightedLetterFromLetterTypeWeights = (letterTypeWeights: IFullLetterTypeWeights) => {
-    const random = randomNumber({ min: 1, max: 100 });
-    console.info('What is random number?', random);
-    let letterType!: TLetterTypes;
-    for (const weightName in letterTypeWeights) {
-      const weightType = weightName as TLetterTypes;
-      const weight = letterTypeWeights[weightType];
-      const { start, end } = weight;
-      console.info('random', random, 'start', start, 'random >= start', random >= start);
-      console.info( 'random', random, 'end', end, 'random <= end', random <= end);
-      if (random >= start && random <= end) {
-        letterType = weightType;
-        console.info(`generating a letter that is a ${weightType}`)
-        break;
-      }
-    }
-
-    const randomLetter = getRandomLetterFromLetterType(letterType);
-    return randomLetter;
-  }
 
   const getInitialLetterFromLetterType = () => {
     const randomInitialLetter = generateWeightedLetterFromLetterTypeWeights(letterTypeWeightedTowardConsonant).toUpperCase()
